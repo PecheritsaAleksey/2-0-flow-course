693b2c7a1cd28ae16c25868d7b9893f68481e6df
Aleksey Pecheritsa <fromzeya@gmail.com>
2024-10-03 09:16:54 +0700

    Lesson-3

--- Source/Module 0/notes-manager/app.js
+++ Source/Module 0/notes-manager/app.js
@@ -1,1 +1,5 @@
-console.log("Hello, World!");
+const userName = "Алексей";
+// console.log(userName + ", добро пожаловать в Консольный Менеджер Заметок!");
+console.log(`${userName}, добро пожаловать в Консольный Менеджер Заметок!`);
+
+let userInput;
