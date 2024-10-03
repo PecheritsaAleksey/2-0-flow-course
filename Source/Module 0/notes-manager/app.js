46097d286e8ce445eb13e1a41fe64187de6778a3
Aleksey Pecheritsa <fromzeya@gmail.com>
2024-10-03 09:26:07 +0700

    Lesson-4

--- Source/Module 0/notes-manager/app.js
+++ Source/Module 0/notes-manager/app.js
@@ -1,5 +1,18 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
 const userName = "Алексей";
-// console.log(userName + ", добро пожаловать в Консольный Менеджер Заметок!");
 console.log(`${userName}, добро пожаловать в Консольный Менеджер Заметок!`);
 
 let userInput;
+
+rl.question("Введите число: ", (input) => {
+  userInput = input;
+  console.log(`Ваше число: ${userInput}`);
+
+  rl.close();
+});
