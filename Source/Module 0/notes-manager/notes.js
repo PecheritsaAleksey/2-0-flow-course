e15f72bf1661b5ee17161e37f3fb26c6b4a82dcc
Aleksey Pecheritsa <fromzeya@gmail.com>
2024-10-04 17:30:51 +0700

    Lesson-13

--- Source/Module 0/notes-manager/notes.js
+++ Source/Module 0/notes-manager/notes.js
@@ -1,10 +1,30 @@
+const fs = require("fs");
+
 let notes = [];
 
+function loadNotes() {
+  try {
+    const dataBuffer = fs.readFileSync("notes.json");
+    const dataJSON = dataBuffer.toString();
+    notes = JSON.parse(dataJSON);
+  } catch (e) {
+    notes = [];
+  }
+}
+
+function saveNotes() {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync("notes.json", dataJSON);
+}
+
 function addNote(title, content) {
+  loadNotes();
   notes.push({ title, content });
+  saveNotes();
 }
 
 function viewNotes() {
+  loadNotes();
   if (notes.length === 0) {
     console.log("У вас нет заметок.");
   }
@@ -15,6 +35,7 @@ function viewNotes() {
 }
 
 function readNote(title) {
+  loadNotes();
   const note = notes.find((note) => note.title === title);
   if (note) {
     console.log(`\nЗаголовок: ${note.title}`);
@@ -25,9 +46,11 @@ function readNote(title) {
 }
 
 function editNote(title, newContent) {
+  loadNotes();
   const noteIndex = notes.findIndex((note) => note.title === title);
   if (noteIndex !== -1) {
     notes[noteIndex].content = newContent;
+    saveNotes();
     console.log("Заметка обновлена.");
   } else {
     console.log("Заметка не найдена.");
@@ -35,12 +58,15 @@ function editNote(title, newContent) {
 }
 
 function deleteNote(title) {
+  loadNotes();
   const notesToKeep = notes.filter((note) => note.title !== title);
   notes = notesToKeep;
+  saveNotes();
   console.log("Заметка удалена.");
 }
 
 function searchNotes(keyword) {
+  loadNotes();
   const filteredNotes = notes.filter((note) => {
     return note.title.includes(keyword) || note.content.includes(keyword);
   });
